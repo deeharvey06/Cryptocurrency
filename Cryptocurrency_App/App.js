4b9a3dab48c1270221fa67bc5bbee2036a9140db
Clifford <deeharvey06@yahoo.com>
2018-01-02 22:06:46 -0800

    hearder

---
+++ Cryptocurrency_App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Header } from './src/compnonets'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Header/>
+      </View>
+    );
+  }
+}
